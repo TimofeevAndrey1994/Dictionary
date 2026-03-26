680062be5fbd6fa16efd54f2183a05183cb1f473
TimofeevAndrey1994 <31769097+TimofeevAndrey1994@users.noreply.github.com>
2026-03-26 15:25:46 +0300

    Remove XML material dependency and use platform app themes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(libs.material)
 
     implementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(platform(libs.androidx.compose.bom))
